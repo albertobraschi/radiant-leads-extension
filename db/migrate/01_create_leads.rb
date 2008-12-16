8c670a921cf1ccbbdeaaae71ae94b0d8da9647dd
jhulten <jhulten@gmail.com>
2008-12-16 16:59:25 -0700

    Added lead and lead_status migrations.

--- db/migrate/01_create_leads.rb
+++ db/migrate/01_create_leads.rb
@@ -1,7 +1,21 @@
 class CreateLeads < ActiveRecord::Migration
   def self.up
-    create_table :leads do |t|
+    create_table :lead_statuses do |t|
+      t.column :name, :string
+    end
 
+    LeadStatus.create('New')
+    LeadStatus.create('Seen')
+    LeadStatus.create('Delivered')
+    LeadStatus.create('Failed')
+    LeadStatus.create('Rejected')
+
+    create_table :leads do |t|
+      t.column :first_name, :string
+      t.column :last_name, :string, :null => false
+      t.column :phone, :string, :limit => 32
+      t.column :email, :string, :null => false
+      t.column :status_id, 
       t.timestamps
     end
   end
