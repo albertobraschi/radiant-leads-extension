76987cd5babce63e9920c24714a7df24df01aff7
Jeffrey Hulten <jeffh@host002.tragicallyleet.com>
2008-12-16 18:27:44 -0500

    added base class descriptor

---
+++ leads_extension.rb
@@ -0,0 +1,7 @@
+
+class LeadsExtension < Radiant::Extension
+  version "0.1"
+  description %{ Enables the collection of leads and forwarding them to CRM systems. }
+  url "http://github.com/jhulten/radiant-leads-extension"
+ 
+end
